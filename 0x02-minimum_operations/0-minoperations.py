36771f539b600f72063ece8c8a5b668b1cb8b1fc
Yahia <yn7599@gmail.com>
2024-07-12 01:08:45 +0300

    0x02: minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Calculation of minimum operations in a situation
+you have a file containing one letter X
+the only operations you can do to this file is "Copy All", "Paste"
+
+given a number `n` what is the minimum number of operations
+needed to have `n` X in the file
+"""
+
+
+def minOperations(n):
+    """get the minimum number of operations"""
+    return sum(primeFactors(n))
+
+
+def primeFactors(n):
+    """Get a list of all prime factors for a number"""
+    factors = []
+    if n < 2:
+        return factors
+    for prime in primeGenerator():
+        while n % prime == 0:
+            n /= prime
+            factors.append(prime)
+        if n == 1:
+            break
+    return factors
+
+
+def primeGenerator():
+    """prime numbers generator"""
+    n = 2
+    primes = []
+    while True:
+        for prime in primes:
+            if n % prime:
+                continue
+        yield n
+        primes.append(n)
+        n += 1
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,11 @@
+if you have a file containing one letter X
+the only operations you can do to this file is "Copy All", "Paste"
+
+given a number `n` what is the minimum number of operations needed to have the letter X in the file `n` times 
+
+try to see a pattern then have a look at this links
+
+## Links That might help
+[Prime Factorization (Khan Academy)](https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization)
+
+[Functions (Python Documentation)](https://docs.python.org/3/tutorial/controlflow.html#defining-functions)
