1771330a2e1334614d11ad1fb0ad038ccde2e5bb
Yahia <yn7599@gmail.com>
2024-08-06 14:56:49 +0300

    space before function paren!

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
-async function requestAsync(url) {
+async function requestAsync (url) {
   return new Promise((resolve, reject) => {
     request(url, function (err, _, body) {
       if (err) return reject(err);
@@ -10,7 +10,7 @@ async function requestAsync(url) {
     });
   });
 }
-async function main() {
+async function main () {
   const film = await requestAsync(baseUrl + process.argv[2]);
   for (const url of film.characters) {
     const character = await requestAsync(url);
