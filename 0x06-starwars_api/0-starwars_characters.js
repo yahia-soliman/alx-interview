0204c6f0ada5cf9b54a28db00c856bec5d6b6f5b
Yahia <yn7599@gmail.com>
2024-08-06 14:24:43 +0300

    in the same order!

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const baseUrl = 'https://swapi-api.alx-tools.com/api/';
-request(baseUrl + 'films/' + process.argv[2], function (error, response, body) {
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+request(baseUrl + process.argv[2], function (error, _, body) {
   if (error) return console.error(error);
-  const data = JSON.parse(body);
-  for (const url of data.characters) {
-    request(url, function (err, res, body) {
-      if (err) return console.error(err);
-      console.log(JSON.parse(body).name);
+  const re = /"characters":\[(.*)\],"planets":/;
+  const characters = re.exec(body)[1].replaceAll('"', '').split(',');
+  for (const url of characters) {
+    request(url, function (err, _, body) {
+      if (!err) console.log(JSON.parse(body).name);
     });
   }
 });
