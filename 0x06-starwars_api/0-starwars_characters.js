994247719e8bb9371bb4cdd17012557cbdf1a44e
Yahia <yn7599@gmail.com>
2024-08-06 14:51:35 +0300

    request Async

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
-request(baseUrl + process.argv[2], function (error, _, body) {
-  if (error) return console.error(error);
-  const re = /"characters":\[(.*)\],"planets":/;
-  const characters = re.exec(body)[1].replaceAll('"', '').split(',');
-  for (const url of characters) {
+
+async function requestAsync(url) {
+  return new Promise((resolve, reject) => {
     request(url, function (err, _, body) {
-      if (!err) console.log(JSON.parse(body).name);
+      if (err) return reject(err);
+      resolve(JSON.parse(body));
     });
+  });
+}
+async function main() {
+  const film = await requestAsync(baseUrl + process.argv[2]);
+  for (const url of film.characters) {
+    const character = await requestAsync(url);
+    console.log(character.name);
   }
-});
+}
+main();
