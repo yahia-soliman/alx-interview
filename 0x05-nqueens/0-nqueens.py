ca6eb3564826e8b6cc217e46fb2d3820d8840ac1
Yahia <yn7599@gmail.com>
2024-08-11 11:47:43 +0300

    nqueens recursion

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -16,29 +16,40 @@ def n_from_argv():
     return N, None
 
 
-def attacable(place, queenplace):
+def attacable(place, queen):
     """Check if a place is attacable by a queen"""
-    xdiff = abs(place[0] - queenplace[0])
-    ydiff = abs(place[1] - queenplace[1])
+    xdiff = abs(place[0] - queen[0])
+    ydiff = abs(place[1] - queen[1])
     return xdiff == ydiff or xdiff == 0 or ydiff == 0
+
+
+def solve(N, available):
+    """Solve the nqueens problem recursively"""
+    if N == 0:
+        return [[]]
+    if len(available) < N:
+        return []
+
+    solutions = []
+
+    for queen in available:
+        avail = [p for p in available if not attacable(p, queen)]
+        result = solve(N - 1, avail)
+        for row in result:
+            if any([point in sol for point in row for sol in solutions]):
+                continue
+            row.insert(0, queen)
+            solutions.append(row)
+
+    return solutions
 
 
 def nqueens(N):
     """get every possible solution to place
     N non-attacking queens on an NxN chessboard
     """
-    solutions = []
     board = [[i, j] for i in range(N) for j in range(N)]
-    for place in board[: N // 2 + 1]:
-        queens = [place]
-        for p in board:
-            if any([attacable(p, queen) for queen in queens]):
-                continue
-            queens.append(p)
-        if len(queens) == N:
-            solutions.append(queens)
-
-    return solutions
+    return solve(N, board)
 
 
 if __name__ == "__main__":
