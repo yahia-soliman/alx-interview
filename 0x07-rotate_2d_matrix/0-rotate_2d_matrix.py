cdd400e625b515bb43b4057de5dc7296338907c4
Yahia <yn7599@gmail.com>
2024-08-14 01:27:13 +0300

    rotate 2d matrix: better impl

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,9 +3,18 @@
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate `n x n` 2D matrix"""
+    """Rotate `n x n` 2D matrix
+
+    a  b  c       h  e  a
+    e  f  g  -->  i  f  b
+    h  i  j       j  g  c
+    """
     n = len(matrix)
-    copy = [[i for i in row] for row in matrix]
-    for i in range(n):
-        for j in range(n):
-            matrix[j][n - i - 1] = copy[i][j]
+
+    for x in range(n // 2):
+        for y in range(x, n - x - 1):
+            tmp = matrix[x][y]
+            matrix[x][y] = matrix[n - 1 - y][x]
+            matrix[n - 1 - y][x] = matrix[n - 1 - x][n - 1 - y]
+            matrix[n - 1 - x][n - 1 - y] = matrix[y][n - 1 - x]
+            matrix[y][n - x - 1] = tmp  # matrix[x][y]
